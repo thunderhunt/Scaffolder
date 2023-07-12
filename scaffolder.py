cb2242db3323d0a095a016db22ed07a42c79ddef
Rahan Sharma <rahan.sharma97@gmail.com>
2023-07-12 11:56:00 +0530

    updated the files to install as cmd tool

--- scaffolder.py
+++ scaffolder.py
@@ -17,23 +17,26 @@ def create_folder_structure(folder_structure, parent_path=''):
                     f.close()
 
 
+def main():
+    parser = argparse.ArgumentParser(description='Generate the scaffolding for the project')
+    parser.add_argument('-p', '--path', help='Path to generate the scaffolding at')
+    parser.add_argument("-n",'--name',help="Name of the project")
+    parser.add_argument("-c","--config",help="Configuration File in YAML")
+    args = vars(parser.parse_args())
+    path=''
+    config_path="./scaffolding.yml"
+    if args['path'] is not None:
+        path = args['path']
+    if args['config'] is not None:
+        config_path=args['config']
+    if args['name'] is not None:
+        print(f"Creating Project Directory - {args['name']} at Path - {path}")
+        path = os.path.join(path, args['name'])
+        os.makedirs(path, exist_ok=True)
 
-parser = argparse.ArgumentParser(description='Generate the scaffolding for the project')
-parser.add_argument('-p', '--path', help='Path to generate the scaffolding at')
-parser.add_argument("-n",'--name',help="Name of the project")
-parser.add_argument("-c","--config",help="Configuration File in YAML")
-args = vars(parser.parse_args())
-path=''
-config_path="./scaffolding.yml"
-if args['path'] is not None:
-    path = args['path']
-if args['config'] is not None:
-    config_path=args['config']
-if args['name'] is not None:
-    print(f"Creating Project Directory - {args['name']} at Path - {path}")
-    path = os.path.join(path, args['name'])
-    os.makedirs(path, exist_ok=True)
+    with open(config_path, 'r') as file:
+        folder_structure = yaml.safe_load(file)
+    create_folder_structure(folder_structure,path)
 
-with open(config_path, 'r') as file:
-    folder_structure = yaml.safe_load(file)
-create_folder_structure(folder_structure,path)
+if __name__=='__main__':
+    main()
--- setup.py
+++ setup.py
@@ -1,17 +1,18 @@
 import setuptools
 
-with open("README.md", "r") as fh:
-    long_description = fh.read()
-
 setuptools.setup(
     name="scaffolder",
     version="1.0.0",
     author="Rahan Sharma",
+    packages=[],
+    entry_points={
+        'console_scripts':[
+            'scaffolder=scaffolder:main'
+        ]
+    },
     description="Tool used to create scaffolding, based on the supplied configuration YAML.",
-    long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/Thunderhunt/scaffolder",
-    packages=setuptools.find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
